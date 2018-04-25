22ff7dab308966fe755e91e091759f20833297be
Tim Almamedov <tim.almamedov@gmail.com>
2018-04-25 10:21:27 +0200

    basic 1 done

---
+++ Basic/script.js
@@ -0,0 +1,31 @@
+var library = [
+  {
+    title: "When Breath Becomes Air",
+    author: "Paul Kalanithi",
+    readingStatus: true
+  },
+  { title: "The Girls", author: "Emma Cline ", readingStatus: true },
+  { title: "The Nest", author: "Cynthia", readingStatus: false },
+  { title: "The Crown", author: "Kiera Cass", readingStatus: true },
+  { title: "Behind Closed Doors", author: "B.A. Paris", readingStatus: false },
+  { title: "It Ends with Us ", author: "Colleen Hoover", readingStatus: true }
+];
+
+function read(args) {
+  args.forEach(function(e) {
+    if (e.readingStatus == true) {
+      // console.log("Already read: " + e.title);
+      document.getElementById(
+        "books"
+      ).innerHTML += `<p style=\"color: green;\" >Already read: ${e.title}`;
+    } else {
+      document.getElementById(
+        "books"
+      ).innerHTML += `<p style=\"color: red;\">You still need to read: ${
+        e.title
+      }`;
+    }
+  });
+}
+
+read(library);
